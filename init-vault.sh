18d7e588fcb75b3199ab77265db47491eb60498c
BHARATH KUMAR DEVARAJU <bkdevara@au1.ibm.com>
2025-05-19 13:08:34 +1000

    updated file names

--- init-vault.sh
+++ init-vault.sh
@@ -1,3 +1,17 @@
-sudo yum install -y yum-utils
-sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
-sudo yum -y install vault
+export VAULTROOT="$HOME/vault"
+vault server -config=$(VAULTROOT)/vault-server.hcl
+
+sleep 5
+echo "Initialize unseal key and root token (please save it for later use)"
+
+export VAULT_ADDR=https://$(hostname -i):8200
+
+#skip tls verify since self-signed certificate is used 
+
+export VAULT_SKIP_VERIFY=true
+
+vault operator init -key-shares=1 -key-threshold=1
+
+#To unseal your vault use the following command 
+# vault operator unseal
+
