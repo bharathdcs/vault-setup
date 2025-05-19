0c1d1cb2fdcb310b662ceff4dfc852bd0720b7ad
BHARATH KUMAR DEVARAJU <bkdevara@au1.ibm.com>
2025-05-19 13:20:46 +1000

    updated scripts to run as a daemon

--- configure.sh
+++ configure.sh
@@ -1,17 +1,17 @@
-export VAULTROOT="$HOME/vault"
+export VAULT_CONFIG=/etc/vault.d
 echo "Creating the vault data directory"
-mkdir -p $VAULTROOT/vault-data
+mkdir -p $VAULT_CONFIG
 
 sleep 10
 echo "Generating the certificates"
 openssl req -x509 -newkey rsa:4096 -sha256 -days 365 \
-    -nodes -keyout $VAULTROOT/vault-key.pem -out $VAULTROOT/vault-cert.pem \
+    -nodes -keyout $VAULT_CONFIG/vault-key.pem -out $VAULT_CONFIG/vault-cert.pem \
     -subj "/CN=$(hostname -f)" \
     -addext "subjectAltName=DNS:localhost,IP:127.0.0.1"
 
 echo "Preparing the configuration...."
 sleep 10
-cat > $VAULTROOT/vault-server.hcl << EOF
+cat > $VAULT_CONFIG/vault-server.hcl << EOF
 api_addr                = "https://$(hostname -i):8200"
 cluster_addr            = "https://$(hostname -i):8201"
 cluster_name            = "learn-vault-cluster"
@@ -20,12 +20,12 @@ ui                      = true
 
 listener "tcp" {
 address       = "$(hostname -i):8200"
-tls_cert_file = "$VAULTROOT/vault-cert.pem"
-tls_key_file  = "$VAULTROOT/vault-key.pem"
+tls_cert_file = "$VAULT_CONFIG/vault-cert.pem"
+tls_key_file  = "$VAULT_CONFIG/vault-key.pem"
 }
 
 backend "raft" {
-path    = "$VAULTROOT/vault-data"
+path    = "$VAULT_CONFIG/vault-data"
 node_id = "learn-vault-server"
 }
 EOF
--- init-vault.sh
+++ init-vault.sh
@@ -1,8 +1,45 @@
-export VAULTROOT="$HOME/vault"
-vault server -config=$(VAULTROOT)/vault-server.hcl
+export VAULT_CONFIG=/etc/vault.d
+export VAULT_BINARY=$(which vault)
 
+echo "Create vault service daemon"
 sleep 5
+sudo tee /lib/systemd/system/vault.service <<EOF
+[Unit]
+Description="HashiCorp Vault"
+Documentation="https://developer.hashicorp.com/vault/docs"
+ConditionFileNotEmpty="${VAULT_CONFIG}/vault-server.hcl"
+
+[Service]
+User=vault
+Group=vault
+SecureBits=keep-caps
+AmbientCapabilities=CAP_IPC_LOCK
+CapabilityBoundingSet=CAP_SYSLOG CAP_IPC_LOCK
+NoNewPrivileges=yes
+ExecStart=${VAULT_BINARY} server -config=${VAULT_CONFIG}/vault-server.hcl
+ExecReload=/bin/kill --signal HUP
+KillMode=process
+KillSignal=SIGINT
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+sudo chmod 644 /lib/systemd/system/vault.service
+
+echo "Start vault service daemon"
+
+sudo systemctl daemon-reload
+
+sudo systemctl start vault.service
+
+echo "verify the status of vault service"
+sleep 5
+systemctl status vault.service
+
+
 echo "Initialize unseal key and root token (please save it for later use)"
+sleep 5
 
 export VAULT_ADDR=https://$(hostname -i):8200
 
